500fedd885f429026fda0561be55c455517536c7
Nivas <nivas@think42labs.com>
2018-08-27 15:33:29 +0530

    Created branch as 10.0

---
+++ hide_import_button/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Import Button - Based on User Groups',
+    'summary': """
+        Displaying Import Button based on user group.""",
+    'version': '10.0.1.2',
+    'license': 'AGPL-3',
+    'author': 'Nivas M',
+    'website': '',
+    'depends': ['base', 'web'],
+    'data': ['security/security.xml',
+             'views/webclient_templates.xml'
+    ],
+    'installable': True,
+}
---
+++ hide_import_button/static/src/js/hide_import_button.js
@@ -0,0 +1,23 @@
+odoo.define('hide_import_ button.hide_import_ button', function (require) {
+'use strict';
+
+var Model = require('web.DataModel');
+var ListView = require('web.ListView');
+
+ListView.include({
+    render_buttons: function() {
+        var self = this;
+        this._super.apply(this, arguments);
+
+        var Users = new Model('res.users');
+        Users.call('has_group', ['hide_import_button.show_import_button']).done(function(belongs_to_group) {
+	      	if (belongs_to_group == false) {
+	      		self.$buttons.find('.o_button_import').remove();
+	      	}
+	    });
+        
+        return this.$buttons;
+    },
+});
+
+});
