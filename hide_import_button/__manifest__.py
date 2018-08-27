711d366295efdc7c935bb64c1f8826d0010d254b
Nivas <nivas@think42labs.com>
2018-08-27 16:27:00 +0530

    Added description for module.

--- hide_import_button/__manifest__.py
+++ hide_import_button/__manifest__.py
@@ -1,15 +1,23 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Import Button - Based on User Groups',
-    'summary': """
-        Displaying Import Button based on user group.""",
+    'name': 'Show Import Button - Based on User Groups',
     'version': '10.0.1.2',
-    'license': 'AGPL-3',
+    'license': 'LGPL-3',
+    'category': 'Tools',
+    "sequence": 1,
+    'summary': 'This module is used for restrict the import button for user level access.',
+    'complexity': "easy",
+    'description': """
+        This module hide the 'Import' button by default.
+        If you want to show 'Import' button for a specific user you have to select 'Show Import Button' group in user form.
+
+    """,
     'author': 'Nivas M',
-    'website': '',
     'depends': ['base', 'web'],
     'data': ['security/security.xml',
              'views/webclient_templates.xml'
     ],
     'installable': True,
+    'auto_install': False,
+    'application': True,
 }
