25f928cfb1da19350c5ad5f451d7813946565925
Nivas <nivas@think42labs.com>
2018-08-27 15:00:22 +0530

    Added odoo9 import button module.

---
+++ hide_import_button/__openerp__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Import Button - Based on User Groups',
+    'summary': """
+        Displaying Import Button based on user group.""",
+    'version': '9.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Nivas M',
+    'website': '',
+    'depends': ['base', 'web'],
+    'data': ['security/security.xml',
+             'views/webclient_templates.xml'
+    ],
+}
